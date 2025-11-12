e91f4b9386356e56b2c17af4bdeb3435ed722b2d
ajguata <aagouan@gmail.com>
2025-11-12 16:11:21 +0000

    maj

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { TetrisGame } from "./components/TetrisGame";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-purple-900 via-blue-900 to-black flex items-center justify-center p-4">
+    <div className="min-h-screen bg-gradient-to-br from-black to-blue-900 flex items-center justify-center p-4">
       <TetrisGame />
     </div>
   );
